ddef19de597b7c68c140ae6a1d72f977d76c693a
myautomateteam-ship-it <myautomate.team@gmail.com>
2026-03-15 17:13:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import asyncio
-import sys
-import os
-
+import asyncio, sys, os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
 from bot.handlers import main
-
 if __name__ == "__main__":
     asyncio.run(main())

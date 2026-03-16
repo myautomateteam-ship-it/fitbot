0a7eeffd4d5079f16d9dedc7e6d62cab0f151670
myautomateteam-ship-it <myautomate.team@gmail.com>
2026-03-16 17:26:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import asyncio, sys, os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from bot.handlers import main
+
+from bot.handlers import dp, bot, scheduler
+
+async def main():
+    print("🤖 FitBot запущен!")
+    asyncio.create_task(scheduler())
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
     asyncio.run(main())
